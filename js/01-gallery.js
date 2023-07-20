aa434cbf0a677a1618f9cb361b64a2ca697b80ed
MichaelTetris <125746441+MichaelTetris@users.noreply.github.com>
2023-07-20 23:20:56 +0300

    task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+console.log(galleryEl);
+
+const galleryList = galleryItems
+  .map(
+    ({ preview, original, description }) => `
+<li class="item-gallery">
+    <a class="gallery-link" href="${original}">
+      <img
+      class="gallery-img"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+      />
+    </a>
+</li>`
+  )
+  .join("");
+
+galleryEl.insertAdjacentHTML("beforeend", galleryList);
+
+galleryEl.onclick = (event) => {
+  if(event.target.tagName !== "IMG") {
+    return;
+  }
+  event.preventDefault();
+  
+  const instance = basicLightbox.create(`
+  <img width="800" height="600" src="${event.target.dataset.source}">
+`, {
+      onShow: () => {
+          document.addEventListener('keydown', closeModal);
+      },
+      onClose: () => {
+          document.removeEventListener('keydown', closeModal);
+      },
+  });
+  instance.show();
+  
+      function closeModal(event) {
+     if (event.code !== 'Escape') {
+         return;
+     } 
+          instance.close();
+  }
+}
