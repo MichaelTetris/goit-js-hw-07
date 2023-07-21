8da0d09611089cfc2278c3e63f74546d9b39d20b
MichaelTetris <125746441+MichaelTetris@users.noreply.github.com>
2023-07-21 22:42:22 +0300

    task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,28 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+const galleryList = galleryItems
+  .map(
+    ({ preview, original, description }) => `
+<li class="item-gallery">
+    <a class="gallery-link" href="${original}">
+      <img
+      class="gallery-img"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+      />
+    </a>
+</li>`
+  )
+  .join("");
+
+galleryEl.insertAdjacentHTML("afterbegin", galleryList);
+
+new SimpleLightbox('.gallery a', {
+  captionData: 'alt',
+  captionDelay: 250,
+});
